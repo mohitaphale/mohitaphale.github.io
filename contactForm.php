527a719e057ba3f999ab34afbaee0b90084b2f3d
mohitaphale <mohitaphale@gmail.com>
2020-12-28 15:44:50 +0530

    revert contact form changes

--- contactForm.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if(isset($_POST['contactName']) && isset($_POST['contactEmail']) && isset($_POST['contactSubject']) && isset($_POST['contactMessae'])) {
-        $data = $_POST['contactName'] . '-' . $_POST['contactEmail'] . $_POST['contactSubject'] . '-' . $_POST['contactMessae'] . "\r\n";
-        $ret = file_put_contents('contactData.txt', $data, FILE_APPEND | LOCK_EX);
-    }
-?>
