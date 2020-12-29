a3bdea3b48e86a38746a4d9caa237d9c96be2aaa
mohitaphale <mohitaphale@gmail.com>
2020-12-29 22:10:41 +0530

    Update main.js
    Website loader - chnage slow animation to fast.

--- js/main.js
+++ js/main.js
@@ -15,10 +15,10 @@
    $(window).load(function() {
 
       // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+    	$("#loader").fadeOut("fast", function(){
 
         // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
+        $("#preloader").delay(100).fadeOut("fast");
 
       });       
 
